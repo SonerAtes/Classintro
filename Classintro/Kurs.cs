082ddc740d3805f7a1fef6e3de8a1a3dbcebf61e
Soner ATEŞ <captain_REX_camino@hotmail.com>
2021-03-07 23:19:10 +0300

    Add project files.

---
+++ Classintro/Kurs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classintro
+{
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ Classintro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Classintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Soner";
+            int yas = 22;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Soner ATEŞ";
+            kurs1.IzlenmeOrani = 34;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "HTML";
+            kurs2.Egitmen = "Soner ATEŞ";
+            kurs2.IzlenmeOrani = 99;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "CSS";
+            kurs3.Egitmen = "Soner ATEŞ";
+            kurs3.IzlenmeOrani = 85;
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + ":" + kurs.Egitmen + ":" + kurs.IzlenmeOrani);
+            }
+        }
+    }
+}
